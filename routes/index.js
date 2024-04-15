a071459f52e26bd0999ee8016aa453b2d77c1c0c
Olha Prykhodko <o.u.prihodko@gmail.com>
2024-04-15 12:42:09 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 document
   .guerySelector('.search-icon-button')
   .addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent the form from submitting
-    // Perform any additional actions if needed
-
-    // Submit the form programmatically
+    event.preventDefault();
     this.submit();
   });
